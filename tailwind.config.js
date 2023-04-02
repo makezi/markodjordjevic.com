8c932696c56885bb62d10cc1655e4a03c14913d1
Marko Djordjevic <makezi08@gmail.com>
2023-04-03 07:42:20 +1000

    Add tailwind (#2)
    * Add tailwindcss
    
    * Add tailwindcss eslint plugin again, remove some TS rules
    
    * Add next/font
    
    * Update metadata

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './content/**/*.mdx'
+  ],
+  theme: {},
+  plugins: []
+};
