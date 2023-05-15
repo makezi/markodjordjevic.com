85e47922dc6e68972183127f9836b1a316c35c09
Marko Djordjevic <makezi08@gmail.com>
2023-05-15 17:36:03 +1000

    Add Callout and images

--- app/blog/_components/mdx-components.tsx
+++ app/blog/_components/mdx-components.tsx
@@ -1,7 +1,35 @@
 import Image from 'next/image';
 import { useMDXComponent } from 'next-contentlayer/hooks';
+import { CrossIcon } from '@/components/icons';
 
 import { cn } from '@/lib/utils';
+
+function Callout({
+  icon,
+  type,
+  children,
+  className,
+  ...props
+}: {
+  icon?: string;
+  type?: 'default' | 'warning' | 'error';
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div
+      className={cn(
+        'flex items-start rounded-r-lg border-l-4 [&>p]:my-0 p-4',
+        { 'border-red-700 bg-red-50/50 text-red-700': type === 'error' },
+        className
+      )}
+      {...props}
+    >
+      {icon ? <span className="mr-2 text-2xl mt-0.5">{icon}</span> : null}
+      {children}
+    </div>
+  );
+}
 
 export function MDXComponents({ code }: { code: string }) {
   const Component = useMDXComponent(code);
@@ -15,6 +43,8 @@ export function MDXComponents({ code }: { code: string }) {
             {...props}
           />
         ),
+        Callout,
+        CrossIcon,
         Image
       }}
     />
