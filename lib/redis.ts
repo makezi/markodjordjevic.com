b47a207e65f403a44a715ec69a81131db09a85b7
Marko Djordjevic <makezi08@gmail.com>
2023-05-16 11:59:25 +1000

    Update redis url

--- lib/redis.ts
+++ lib/redis.ts
@@ -3,6 +3,6 @@ import { Redis } from '@upstash/redis';
 import env from './env';
 
 export const redis = new Redis({
-  url: 'https://proven-mollusk-33857.upstash.io',
+  url: 'https://sensible-grizzly-34633.upstash.io',
   token: env.UPSTASH_REDIS_REST_TOKEN
 });
